9938fbb0ceb39643e49c437190ad33778bcdd563
Austin Rooke <arooke@broadleafcommerce.com>
2016-03-28 16:16:12 -0500

    Some of the configuration for getting the BroadleafCommercePrivate feature-thymeleaf-3 branch to work. You will need to specify your database information in the build, and also make sure that you set the dialects in common.

--- core/src/main/java/com/mycompany/sample/vendor/nullPaymentGateway/web/processor/NullPaymentGatewayHostedActionProcessor.java
+++ core/src/main/java/com/mycompany/sample/vendor/nullPaymentGateway/web/processor/NullPaymentGatewayHostedActionProcessor.java
@@ -19,20 +19,6 @@
  */
 package com.mycompany.sample.vendor.nullPaymentGateway.web.processor;
 
-import org.broadleafcommerce.common.payment.dto.PaymentRequestDTO;
-import org.broadleafcommerce.common.payment.dto.PaymentResponseDTO;
-import org.broadleafcommerce.common.payment.service.PaymentGatewayHostedService;
-import org.broadleafcommerce.common.vendor.service.exception.PaymentException;
-import com.mycompany.sample.vendor.nullPaymentGateway.service.payment.NullPaymentGatewayConstants;
-import org.springframework.stereotype.Component;
-import org.thymeleaf.Arguments;
-import org.thymeleaf.dom.Element;
-import org.thymeleaf.processor.attr.AbstractAttributeModifierAttrProcessor;
-import org.thymeleaf.standard.expression.StandardExpressionProcessor;
-
-import javax.annotation.Resource;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * <p>A Thymeleaf processor that will generate a Mock Hosted Link given a passed in PaymentRequestDTO.</p>
@@ -51,63 +37,63 @@ import java.util.Map;
  *
  * @author Elbert Bautista (elbertbautista)
  */
-@Component("blNullPaymentGatewayHostedActionProcessor")
-public class NullPaymentGatewayHostedActionProcessor extends AbstractAttributeModifierAttrProcessor {
+// @Component("blNullPaymentGatewayHostedActionProcessor")
+public class NullPaymentGatewayHostedActionProcessor /*extends AbstractAttributeModifierAttrProcessor*/ {
 
-    @Resource(name = "blNullPaymentGatewayHostedService")
-    private PaymentGatewayHostedService paymentGatewayHostedService;
-
-    /**
-     * Sets the name of this processor to be used in Thymeleaf template
-     */
-    public NullPaymentGatewayHostedActionProcessor() {
-        super("null_payment_hosted_action");
-    }
-
-    @Override
-    public int getPrecedence() {
-        return 10000;
-    }
-
-    @Override
-    protected Map<String, String> getModifiedAttributeValues(Arguments arguments, Element element, String attributeName) {
-        Map<String, String> attrs = new HashMap<String, String>();
-
-        PaymentRequestDTO requestDTO = (PaymentRequestDTO) StandardExpressionProcessor.processExpression(arguments, element.getAttributeValue(attributeName));
-        String url = "";
-
-        if (requestDTO != null) {
-            if ( element.getAttributeValue("complete_checkout") != null) {
-                Boolean completeCheckout = (Boolean) StandardExpressionProcessor.processExpression(arguments,
-                        element.getAttributeValue("complete_checkout"));
-                element.removeAttribute("complete_checkout");
-                requestDTO.completeCheckoutOnCallback(completeCheckout);
-            }
-
-            try {
-                PaymentResponseDTO responseDTO = paymentGatewayHostedService.requestHostedEndpoint(requestDTO);
-                url = responseDTO.getResponseMap().get(NullPaymentGatewayConstants.HOSTED_REDIRECT_URL).toString();
-            } catch (PaymentException e) {
-                throw new RuntimeException("Unable to Create Null Payment Gateway Hosted Link", e);
-            }
-        }
-
-        attrs.put("action", url);
-        return attrs;
-    }
-
-    @Override
-    protected ModificationType getModificationType(Arguments arguments, Element element, String attributeName, String newAttributeName) {
-        return ModificationType.SUBSTITUTION;
-    }
-
-    @Override
-    protected boolean removeAttributeIfEmpty(Arguments arguments, Element element, String attributeName, String newAttributeName) {
-        return true;
-    }
-
-    @Override
-    protected boolean recomputeProcessorsAfterExecution(Arguments arguments, Element element, String attributeName) {
-        return false;
-    }
+//    @Resource(name = "blNullPaymentGatewayHostedService")
+//    private PaymentGatewayHostedService paymentGatewayHostedService;
+//
+//    /**
+//     * Sets the name of this processor to be used in Thymeleaf template
+//     */
+//    public NullPaymentGatewayHostedActionProcessor() {
+//        super("null_payment_hosted_action");
+//    }
+//
+//    @Override
+//    public int getPrecedence() {
+//        return 10000;
+//    }
+//
+//    @Override
+//    protected Map<String, String> getModifiedAttributeValues(Arguments arguments, Element element, String attributeName) {
+//        Map<String, String> attrs = new HashMap<String, String>();
+//
+//        PaymentRequestDTO requestDTO = (PaymentRequestDTO) StandardExpressionProcessor.processExpression(arguments, element.getAttributeValue(attributeName));
+//        String url = "";
+//
+//        if (requestDTO != null) {
+//            if ( element.getAttributeValue("complete_checkout") != null) {
+//                Boolean completeCheckout = (Boolean) StandardExpressionProcessor.processExpression(arguments,
+//                        element.getAttributeValue("complete_checkout"));
+//                element.removeAttribute("complete_checkout");
+//                requestDTO.completeCheckoutOnCallback(completeCheckout);
+//            }
+//
+//            try {
+//                PaymentResponseDTO responseDTO = paymentGatewayHostedService.requestHostedEndpoint(requestDTO);
+//                url = responseDTO.getResponseMap().get(NullPaymentGatewayConstants.HOSTED_REDIRECT_URL).toString();
+//            } catch (PaymentException e) {
+//                throw new RuntimeException("Unable to Create Null Payment Gateway Hosted Link", e);
+//            }
+//        }
+//
+//        attrs.put("action", url);
+//        return attrs;
+//    }
+//
+//    @Override
+//    protected ModificationType getModificationType(Arguments arguments, Element element, String attributeName, String newAttributeName) {
+//        return ModificationType.SUBSTITUTION;
+//    }
+//
+//    @Override
+//    protected boolean removeAttributeIfEmpty(Arguments arguments, Element element, String attributeName, String newAttributeName) {
+//        return true;
+//    }
+//
+//    @Override
+//    protected boolean recomputeProcessorsAfterExecution(Arguments arguments, Element element, String attributeName) {
+//        return false;
+//    }
 }
